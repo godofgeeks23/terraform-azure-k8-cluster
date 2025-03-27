4e000fa60028e0428f0984cf9c1df1ca4f518b4f
Aviral <aviralji4@gmail.com>
2025-03-27 06:46:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,27 +42,36 @@ variable "vm_count" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "k8s-cluster-rg"
+  name     = "terraform-k8s-cluster-rg"
   location = "centralindia"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "k8s-vnet"
+  name                = "terraform-k8s-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "k8s-subnet"
+  name                 = "terraform-k8s-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "public_ip" {
+  count               = var.vm_count
+  name                = "terraform-k8s-public-ip-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
 resource "azurerm_network_interface" "nic" {
   count               = var.vm_count
-  name                = "k8s-nic-${count.index}"
+  name                = "terraform-k8s-nic-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -70,12 +79,13 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip[count.index].id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   count                 = var.vm_count
-  name                  = "k8s-node-${count.index}"
+  name                  = "terraform-k8s-node-${count.index}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   size                  = "Standard_B2s"
@@ -94,10 +104,16 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
-  # custom_data = filebase64("cloud-init.yaml")
+  custom_data = filebase64("cloud-init.yaml")
+}
+
+output "vm_public_ips" {
+  value = [
+    for ip in azurerm_public_ip.public_ip : ip.ip_address
+  ]
 }
